39a0bf31fe341b76124e93a71c604f1f4e6eb928
hstrejolunaoasis <htrejo@oasishoteles.com>
2022-07-10 21:19:52 -0500

    navbar & assets

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 
-import './Navbar.scss';
-
+import { images } from "../../constants";
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "contact", "work", "skills", "testimonial"].map(
+          (item) => (
+            <li className="app__flex p-text" key={`link-${item}`}>
+              <div />
+
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          )
+        )}
+      </ul>
+    </nav>
   );
 };
 export default Navbar;
