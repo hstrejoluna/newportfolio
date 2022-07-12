98894753f342dd7b158985fa4de396a717104a94
hstrejoluna <trejolunatutoriales@gmail.com>
2022-07-12 12:03:38 -0500

    sanity setup

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,10 +2,14 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "",
+
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
-  apiVersion: "",
+  apiVersion: "2022-12-07",
   useCdn: true,
-  token: "",
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,45 +1,62 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./About.scss";
-import { images } from '../../constants';
+import { images } from "../../constants";
 
 const abouts = [
-  { title: 'Web Development', description: 'I can build your website', imgUrl: images.about01 },
-  { title: 'Web Design', description: 'I can build your website', imgUrl: images.about02 },
-  { title: 'Mobile Design', description: 'I can build your website', imgUrl: images.about03 },
-  { title: 'Web Animations', description: 'I can build your website', imgUrl: images.about04 },
-]
-
+  {
+    title: "Web Development",
+    description: "I can build your website",
+    imgUrl: images.about01,
+  },
+  {
+    title: "Web Design",
+    description: "I can build your website",
+    imgUrl: images.about02,
+  },
+  {
+    title: "Mobile Design",
+    description: "I can build your website",
+    imgUrl: images.about03,
+  },
+  {
+    title: "Web Animations",
+    description: "I can build your website",
+    imgUrl: images.about04,
+  },
+];
 
 const About = () => {
   return (
     <>
-      <h2 className="head-text">
-        I know that 
+      <h2 className='head-text'>
+        I know that
         <span>Good Design</span>
         <br />
-        means 
+        means
         <span>Good Business</span>
       </h2>
 
-      <div className="app__profiles">
+      <div className='app__profiles'>
         {abouts.map((about, index) => (
           <motion.div
-          whileInView={{opacity: 1}}
-          whileHover={{scale: 1.1}}
-          transition={{duration: 0.5, type: 'tween'}}
-          className="app__profile-item"
-          key={about.title + index}
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className='app__profile-item'
+            key={about.title + index}
           >
             <img src={about.imgUrl} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20}}>{about.title}</h2>
-            <p className="p-text" style={{ marginTop: 10}}>{about.description}</p>
-
-
+            <h2 className='bold-text' style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className='p-text' style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
           </motion.div>
         ))}
       </div>
-    </> 
+    </>
   );
 };
 export default About;
