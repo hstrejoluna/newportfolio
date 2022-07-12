8a7d31604c2a152ac283cfff1e913b160b6a5d4e
hstrejoluna <trejolunatutoriales@gmail.com>
2022-07-12 16:12:10 -0500

    night mode

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
-
+import React, { useState, createContext } from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
+
 import "./App.scss";
+
+export const ThemeContext = createContext(null);
+
+const toggleTheme = () => {
+  setTheme((curr) => (curr === "light" ? "dark" : "light"));
+};
 
 const App = () => {
   return (
-    <div className="app" id="light">
+    <div className='app' id={theme}>
       <Navbar />
       <Header />
       <About />
