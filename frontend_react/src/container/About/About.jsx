96297985ef83812cf898c6717d3e2071804c791f
hstrejoluna <trejolunatutoriales@gmail.com>
2022-07-12 12:18:23 -0500

    about connect to sanity

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,31 +2,16 @@ import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./About.scss";
 import { images } from "../../constants";
-
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I can build your website",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Web Design",
-    description: "I can build your website",
-    imgUrl: images.about02,
-  },
-  {
-    title: "Mobile Design",
-    description: "I can build your website",
-    imgUrl: images.about03,
-  },
-  {
-    title: "Web Animations",
-    description: "I can build your website",
-    imgUrl: images.about04,
-  },
-];
+import { urlFor, client } from "../../client";
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
   return (
     <>
       <h2 className='head-text'>
@@ -46,7 +31,7 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>
               {about.title}
             </h2>
