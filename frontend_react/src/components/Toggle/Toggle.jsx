23a3805aaa82267ccc7d59f7a4d6cbe806f342a5
hstrejoluna <trejolunatutoriales@gmail.com>
2022-07-14 13:47:09 -0500

    toggle initial setup

---
+++ frontend_react/src/components/Toggle/Toggle.jsx
@@ -0,0 +1,21 @@
+import { BsMoonStarsFill, BsFillSunFill } from "react-icons/bs";
+
+import './Toggle.scss';
+
+const Toggle = () => {
+  return (
+    <>
+      <div id="darkmode">
+        <input type="checkbox" className="checkbox" id="checkbox" />
+        <label htmlFor="checkbox" className="label">
+          <BsMoonStarsFill color="white" />
+          <BsFillSunFill color="yellow" />
+          <div className="ball"></div>
+        </label>
+      </div>
+    </>
+  );
+}
+
+export default Toggle;
+
