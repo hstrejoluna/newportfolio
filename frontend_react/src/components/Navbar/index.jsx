39f3ab19ac88cb46b2443b304a4d714a66376177
hstrejoluna <trejolunatutoriales@gmail.com>
2022-07-14 17:19:59 -0500

    finished dark mode details

--- frontend_react/src/components/Navbar/index.jsx
+++ frontend_react/src/components/Navbar/index.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+
 import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { motion } from "framer-motion";
 
 import { images } from "../../constants";
 import { Toggle } from "../../components";
 
-
 import "./Navbar.scss";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const theme = useSelector((state) => state.theme);
+
   return (
-    <nav className="app__navbar">
-      <div className="app__navbar-logo">
-        <img src={images.logodark} alt="logo" />
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        {!theme.darkmode ? (
+          <img src={images.logowhite} alt='logo' />
+        ) : (
+          <img src={images.logodark} alt='logo' />
+        )}
       </div>
-      <ul className="app__navbar-links">
+      <ul className='app__navbar-links'>
         {["home", "about", "work", "skills", "testimonial", "contact"].map(
           (item) => (
-            <li className="app__flex p-text" key={`link-${item}`}>
+            <li className='app__flex p-text' key={`link-${item}`}>
               <div />
 
               <a href={`#${item}`}>{item}</a>
@@ -31,7 +38,7 @@ const Navbar = () => {
         <Toggle />
       </div>
 
-      <div className="app__navbar-menu">
+      <div className='app__navbar-menu'>
         <HiMenuAlt4 onClick={() => setToggle(true)} />
 
         {toggle && (
@@ -46,7 +53,7 @@ const Navbar = () => {
                 "about",
                 "work",
                 "skills",
-                 "testimonial",
+                "testimonial",
                 "contact",
               ].map((item) => (
                 <li key={item}>
