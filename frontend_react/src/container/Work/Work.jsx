853608e17b75292e3ed4cbb1c95d662630acc422
hstrejoluna <trejolunatutoriales@gmail.com>
2022-07-16 16:52:32 -0500

    work done now with expand button

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,20 +1,59 @@
-import React, { useState, useEffect } from 'react';
- 
-import { AppWrap, MotionWrap } from '../../wrapper';
-import { urlFor, client } from '../../client';
+import React, { useState, useEffect, useRef } from "react";
 
-import { AiFillEye, AiFillGithub } from 'react-icons/ai';
-import { motion } from 'framer-motion';
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
-import './Work.scss';
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+
+import "./Work.scss";
+
+const MAX_POSSIBLE_HEIGHT = 500;
+
+const styles = {
+  container: {
+    width: 300,
+    margin: "0 auto",
+  },
+  card: {
+    backgroundColor: "#B7E0F2",
+    borderRadius: 55,
+    padding: "3rem",
+  },
+};
+
+const ExpendableText = ({ maxHeight, children }) => {
+  const ref = useRef();
+  const [shouldShowExpand, setShouldShowExpand] = useState(false);
+  const [expanded, setExpanded] = useState(true);
+
+  useEffect(() => {
+    if (ref.current.scrollHeight > maxHeight) {
+      setShouldShowExpand(true);
+      setExpanded(false);
+    }
+  }, [maxHeight]);
+
+  return (
+    <div ref={ref}>
+      <div
+        className='app__inner'
+        style={{ maxHeight: expanded ? MAX_POSSIBLE_HEIGHT : maxHeight }}
+      >
+        {children}
+      </div>
+      {shouldShowExpand && (
+        <button onClick={() => setExpanded(!expanded)}>Expand</button>
+      )}
+    </div>
+  );
+};
 
 const Work = () => {
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
-  const [activeFilter, setActiveFilter] = useState('All');
+  const [activeFilter, setActiveFilter] = useState("All");
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
-
-
 
   useEffect(() => {
     const query = '*[_type == "works"]';
@@ -32,7 +71,7 @@ const Work = () => {
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
-      if (item === 'All') {
+      if (item === "All") {
         setFilterWork(works);
       } else {
         setFilterWork(works.filter((work) => work.tags.includes(item)));
@@ -42,54 +81,61 @@ const Work = () => {
 
   return (
     <>
-      <h2 className="head-text">My Great <span>Portfolio</span> Section</h2>
+      <h2 className='head-text'>
+        My Great <span>Portfolio</span> Section
+      </h2>
 
-      <div className="app__work-filter">
-        {['Back End', 'Front End', 'Mobile', 'WordPress', 'All'].map((item, index) => (
-          <div
-            key={index}
-            onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
-          >
-            {item}
-          </div>
-        ))}
+      <div className='app__work-filter'>
+        {["Back End", "Front End", "Mobile", "WordPress", "All"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text ${
+                activeFilter === item ? "item-active" : ""
+              }`}
+            >
+              {item}
+            </div>
+          )
+        )}
       </div>
 
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__work-portfolio"
+        className='app__work-portfolio'
       >
         {filterWork.map((work, index) => (
-          <div className="app__work-item app__flex" key={index}>
-            <div
-              className="app__work-img app__flex"
-            >
+          <div className='app__work-item app__flex' key={index}>
+            <div className='app__work-img app__flex'>
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-                className="app__work-hover app__flex"
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
+                className='app__work-hover app__flex'
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
-
+                <a href={work.projectLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
-                    className="app__flex"
+                    className='app__flex'
                   >
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                <a href={work.codeLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
-                    className="app__flex"
+                    className='app__flex'
                   >
                     <AiFillGithub />
                   </motion.div>
@@ -97,12 +143,16 @@ const Work = () => {
               </motion.div>
             </div>
 
-            <div className="app__work-content app__flex">
-              <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+            <div className='app__work-content app__flex'>
+              <h4 className='bold-text'>{work.title}</h4>
+              <ExpendableText maxHeight={30}>
+                <p className='p-text' style={{ marginTop: 10 }}>
+                  {work.description}
+                </p>
+              </ExpendableText>
 
-              <div className="app__work-tag app__flex">
-                <p className="p-text">{work.tags[10}</p>
+              <div className='app__work-tag app__flex'>
+                <p className='p-text'>{work.tags[0]}</p>
               </div>
             </div>
           </div>
